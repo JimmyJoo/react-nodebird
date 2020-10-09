d403101b8974c8efa742ae03358704a92143109d
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-09 14:56:33 +0900

    :art: Add NicknameEditForm component in profile page

---
+++ prepare/front/components/NicknameEditForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Form, Input } from 'antd';
+
+const StForm = styled(Form)`
+  margin-bottom: 20px;
+  border: 1px solid #d9d9d9;
+  padding: 10px;
+`;
+
+const NicknameEditForm = () => {
+  return (
+    <StForm>
+      <Input.Search addonBefore="닉네임" enterButton="수정"></Input.Search>
+    </StForm>
+  );
+};
+
+export default NicknameEditForm;
