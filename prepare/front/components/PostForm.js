410838574979e8f5cfde58aa34265d32b35e783c
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-10 19:10:15 +0900

    :lipstick: Add a style to PostForm

---
+++ prepare/front/components/PostForm.js
@@ -0,0 +1,53 @@
+import React, { useCallback, useState } from 'react';
+import { Form, Input, Button } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
+import { addPost } from '../reducers/post';
+
+const PostForm = () => {
+  const dispatch = useDispatch();
+  const { imagePaths } = useSelector((state) => state.post);
+  const [text, setText] = useState('');
+
+  const onChangeText = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    dispatch(addPost());
+  }, []);
+
+  return (
+    <Form
+      style={{ margin: '10px 0 20px' }}
+      encType="multipart/form-data"
+      onFinish={onSubmit}
+    >
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder="어떤 신기한 일이 있었나요?"
+      />
+
+      <div>
+        <input type="file" multiple hidden />
+        <Button>이미지 업로드</Button>
+        <Button type="primary" style={{ float: 'right' }} htmlType="submit">
+          짹짹
+        </Button>
+      </div>
+      <div>
+        {imagePaths.map((image) => (
+          <div key={image} style={{ display: 'inline-block' }}>
+            <img src={image} style={{ width: '200px' }} alt={image} />
+            <div>
+              <Button>제거</Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
+};
+
+export default PostForm;
