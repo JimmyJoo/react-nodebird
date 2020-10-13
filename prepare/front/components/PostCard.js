3aae7429d7d773cb3113f6ad3f0169993cc5f766
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-13 17:12:43 +0900

    :sparkles: Add button groups to PostCard

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Popover, Button } from 'antd';
+import {
+  RetweetOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+} from '@ant-design/icons';
+import { useSelector } from 'react-redux';
+import Avatar from 'antd/lib/avatar/avatar';
+import PostImages from './PostImages';
 
-const PostCard = () => {
-  return <div>PostCard</div>;
+const PostCard = ({ post }) => {
+  const id = useSelector((state) => state.user?.id);
+  return (
+    <div style={{ marginBottom: '20px' }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          <HeartOutlined key="heart" />,
+          <MessageOutlined key="comment" />,
+          <Popover
+            key="more"
+            content={
+              <Button.Group>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button type="danger">삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta avatar={<Avatar>{post.User.nickname[0]}</Avatar>} />
+        {/* <Buttons /> */}
+      </Card>
+      {/* <CommentForm /> */}
+      {/* <Comments /> */}
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.shape({
+      id: PropTypes.number,
+      nickname: PropTypes.string,
+    }),
+    content: PropTypes.string,
+    Images: PropTypes.arrayOf(PropTypes.object),
+    Comments: PropTypes.arrayOf(PropTypes.object),
+    imagePaths: PropTypes.arrayOf(PropTypes.string),
+    postAdded: PropTypes.bool,
+  }).isRequired,
 };
 
 export default PostCard;
