bb5be6eccf5e521b37b56f4e6de69151d74ac157
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-16 12:51:25 +0900

    :lipstick: Add PostImages styles

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
 
 const PostImages = ({ images }) => {
-  return <div>구현중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <img
+        role="presentation"
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%' }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%' }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <img
+        role="presentation"
+        style={{ display: 'inline-block', width: '50%' }}
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+      <div
+        role="presentation"
+        style={{
+          display: 'inline-block',
+          width: '50%',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+          cursor: 'pointer',
+        }}
+        onClick={onZoom}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1}개의 사진 더 보기
+      </div>
+    </>
+  );
 };
 
 PostImages.propTypes = {
