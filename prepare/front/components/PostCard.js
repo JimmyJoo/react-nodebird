92f9823c320ddb39daf9351b7de2e01796eedf48
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-16 12:11:25 +0900

    :sparkles: Add toggle features to comment and like buttons

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Popover, Button } from 'antd';
+import { Card, Popover, Button, Comment, List } from 'antd';
 import {
   RetweetOutlined,
   HeartOutlined,
+  HeartTwoTone,
   MessageOutlined,
   EllipsisOutlined,
 } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
 import Avatar from 'antd/lib/avatar/avatar';
 import PostImages from './PostImages';
+import CommentForm from './CommentForm';
 
 const PostCard = ({ post }) => {
   const id = useSelector((state) => state.user?.id);
+
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+
   return (
     <div style={{ marginBottom: '20px' }}>
       <Card
         cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key="retweet" />,
-          <HeartOutlined key="heart" />,
-          <MessageOutlined key="comment" />,
+          liked ? (
+            <HeartTwoTone
+              key="heart"
+              twoToneColor="#eb2f96"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
           <Popover
             key="more"
             content={
@@ -47,7 +69,25 @@ const PostCard = ({ post }) => {
         />
         {/* <Buttons /> */}
       </Card>
-      {/* <CommentForm /> */}
+      {commentFormOpened && (
+        <div>
+          <CommentForm post={post} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={(item) => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
+        </div>
+      )}
       {/* <Comments /> */}
     </div>
   );
