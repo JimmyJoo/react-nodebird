74c646953059fcc3c887474b96250d3c70bf1433
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-17 11:51:37 +0900

    :sparkles: Show image slider when image is clicked

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
+import ImagesZoom from './imagesZoom';
 
 const PostImages = ({ images }) => {
   const [showImagesZoom, setShowImagesZoom] = useState(false);
@@ -9,14 +10,21 @@ const PostImages = ({ images }) => {
     setShowImagesZoom(true);
   }, []);
 
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
   if (images.length === 1) {
     return (
-      <img
-        role="presentation"
-        src={images[0].src}
-        alt={images[0].src}
-        onClick={onZoom}
-      />
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
     );
   }
 
@@ -37,6 +45,7 @@ const PostImages = ({ images }) => {
           alt={images[1].src}
           onClick={onZoom}
         />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
   }
@@ -65,6 +74,7 @@ const PostImages = ({ images }) => {
         <br />
         {images.length - 1}개의 사진 더 보기
       </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
     </>
   );
 };
