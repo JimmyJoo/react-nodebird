febe1a41638d54c2b39bb377dad52f4ce349fee3
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-18 10:44:21 +0900

    :art: Add PostCardContent component

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -12,6 +12,7 @@ import { useSelector } from 'react-redux';
 import Avatar from 'antd/lib/avatar/avatar';
 import PostImages from './PostImages';
 import CommentForm from './CommentForm';
+import PostCardContent from './PostCardContent';
 
 const PostCard = ({ post }) => {
   const id = useSelector((state) => state.user?.id);
@@ -65,7 +66,9 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={
+            <PostCardContent postData={post.content}></PostCardContent>
+          }
         />
         {/* <Buttons /> */}
       </Card>
