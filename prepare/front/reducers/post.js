e08663c64457c95e1711f5d968ab06e07810ade5
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-21 16:59:33 +0900

    :art: Add comment part to post reducer

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -3,8 +3,14 @@ export const ADD_POST = 'post/ADD_POST';
 export const ADD_POST_SUCCESS = 'post/ADD_POST_SUCCESS';
 export const ADD_POST_FAILURE = 'post/ADD_POST_FAILURE';
 
+export const ADD_COMMENT = 'post/ADD_COMMENT';
+export const ADD_COMMENT_SUCCESS = 'post/ADD_COMMENT_SUCCESS';
+export const ADD_COMMENT_FAILURE = 'post/ADD_COMMENT_FAILURE';
+
 // action creator
 export const addPost = (data) => ({ type: ADD_POST, data });
+
+export const addComment = (data) => ({ type: ADD_COMMENT, data });
 
 // dummyPost
 const dummyPost = {
@@ -59,7 +65,12 @@ const initialState = {
     },
   ],
   imagePaths: [],
-  postAdded: false,
+  addPostLoading: false,
+  addPostDone: false,
+  addPostError: null,
+  addCommentLoading: false,
+  addCommentDone: false,
+  addCommentError: null,
 };
 
 // reducer
@@ -68,8 +79,41 @@ const post = (state = initialState, action) => {
     case ADD_POST:
       return {
         ...state,
+        addPostLoading: true,
+        addPostDone: false,
+        addPostError: null,
+      };
+    case ADD_POST_SUCCESS:
+      return {
+        ...state,
+        addPostLoading: false,
+        addPostDone: true,
         mainPosts: [dummyPost, ...state.mainPosts],
-        postAdded: true,
+      };
+    case ADD_POST_FAILURE:
+      return {
+        ...state,
+        addPostLoading: false,
+        addPostError: action.error,
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        addCommentLoading: true,
+        addCommentDone: false,
+        addCommentError: null,
+      };
+    case ADD_COMMENT_SUCCESS:
+      return {
+        ...state,
+        addCommentLoading: false,
+        addCommentDone: true,
+      };
+    case ADD_COMMENT_FAILURE:
+      return {
+        ...state,
+        addCommentLoading: false,
+        addCommentError: action.error,
       };
     default:
       return state;
