c4ed7b7b581b99fb2c4e172677c191e253eab286
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-23 14:12:51 +0900

    :rotating_light: Change codes to remove linter warnings

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import wrapper from '../store/configureStore';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import withReduxSaga from 'next-redux-saga';
+import wrapper from '../store/configureStore';
 import 'antd/dist/antd.css';
 
-const App = ({ Component }) => {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <title>NodeBird</title>
-      </Head>
-      <Component />
-    </>
-  );
-};
+const App = ({ Component }) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <title>NodeBird</title>
+    </Head>
+    <Component />
+  </>
+);
 
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
