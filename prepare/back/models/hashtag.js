dd5deb2636221bbad4a6478ad89d94ee42f3208b
JimmyJoo <jimmyjoo1029@gmail.com>
2020-10-30 08:51:08 +0900

    :art: Make hashtag model

---
+++ prepare/back/models/hashtag.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Hashtag = sequelize.define(
+    'Hashtag',
+    {
+      name: {},
+    },
+    {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    }
+  );
+
+  Hashtag.associate = (db) => {};
+  return Hashtag;
+};
