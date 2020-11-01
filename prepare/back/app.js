73e8035b4f951ad7312ba095a37e888ee4c2128a
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-01 21:11:56 +0900

    :sparkles: Connect database to server

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const postRouter = require('./routers/post');
-
+const db = require('./models');
 const app = express();
+
+db.sequelize.sync().then(() => {
+  console.log('db 연결 성공');
+});
 
 app.use('/post', postRouter);
 
