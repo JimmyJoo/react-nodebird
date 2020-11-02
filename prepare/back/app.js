e4cf8e46cf3d2ea621a6e8a538d23443a09388fe
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-02 21:44:43 +0900

    :sparkles: Set up signup router in server

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const postRouter = require('./routers/post');
+const userRouter = require('./routers/user');
 const db = require('./models');
 const app = express();
 
@@ -7,7 +8,11 @@ db.sequelize.sync().then(() => {
   console.log('db 연결 성공');
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.listen(3065, () => {
   console.log('서버 실행 중...');
