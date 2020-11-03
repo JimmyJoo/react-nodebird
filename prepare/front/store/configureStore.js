f52647730bb687505dd781c8298fdd7830118c23
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-03 14:17:58 +0900

    :heavy_minus_sign: Remove redux-thunk after testing

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,11 +1,10 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
 import reducer from '../reducers';
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware];
+  const middlewares = [];
   const enhancer =
     process.env.NODE_ENV === 'production'
       ? compose(applyMiddleware(...middlewares))
