4384e1f83b78d8a210bb2c1d0091790651df6eca
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-04 22:23:32 +0900

    :sparkles: Return user data when user logs in successfully

--- prepare/back/passport/local.js
+++ prepare/back/passport/local.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const { Strategy: LocalStrategy } = require('passport-local');
 const bcrypt = require('bcrypt');
-const { User } = require('./model');
+const { User } = require('../models');
 
 module.exports = () => {
   passport.use(
--- prepare/back/routers/user.js
+++ prepare/back/routers/user.js
@@ -1,7 +1,51 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const { User } = require('../models');
+const passport = require('passport');
+
+const { Post, User } = require('../models');
 const router = express.Router();
+
+router.post('/login', (req, res, next) => {
+  console.log('router login: ', req.body);
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.log('router error!');
+      console.error(err);
+      return next(err);
+    }
+    if (info) {
+      console.log('info: ', info);
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, async (loginErr) => {
+      if (loginErr) {
+        console.log('loginErr: ', loginErr);
+        console.error(loginErr);
+        return next(loginErr);
+      }
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: user.id },
+        attributes: {
+          exclude: ['password'],
+        },
+        include: [
+          {
+            model: Post,
+          },
+          {
+            model: User,
+            as: 'Followings',
+          },
+          {
+            model: User,
+            as: 'Followers',
+          },
+        ],
+      });
+      return res.status(200).send(fullUserWithoutPassword);
+    });
+  })(req, res, next);
+});
 
 router.post('/', async (req, res, next) => {
   try {
