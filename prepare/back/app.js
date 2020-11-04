07d885b73e56cada8261111900b6a3f1e36f85e2
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-04 14:38:45 +0900

    :sparkles: Add session and cookie-parser middlewares

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
 const postRouter = require('./routers/post');
 const userRouter = require('./routers/user');
 const db = require('./models');
+const passportConfig = require('./passport');
+
 const app = express();
 
 db.sequelize.sync().then(() => {
   console.log('db 연결 성공');
 });
+
+passportConfig();
 
 app.use(
   cors({
@@ -18,6 +24,10 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/post', postRouter);
 app.use('/user', userRouter);
