23033de756b854b2230fbef46d8ca26bc005f5ff
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-05 10:01:33 +0900

    :rotating_light: Change code style to remove linter warning

--- prepare/front/api/userApi.js
+++ prepare/front/api/userApi.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-export const logInApi = (data) => {
-  return axios.post('/user/login', data);
-};
+export const logInApi = (data) => axios.post('/user/login', data);
 
-export const logOutApi = () => {
-  return axios.post('/user/logout');
-};
+export const logOutApi = () => axios.post('/user/logout');
 
-export const signUpApi = (data) => {
-  return axios.post('http://localhost:3065/user', data);
-};
+export const signUpApi = (data) =>
+  axios.post('http://localhost:3065/user', data);
