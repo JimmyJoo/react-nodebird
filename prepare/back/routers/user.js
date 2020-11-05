63cc82039435549ff40eada57f7225ccc8540011
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-06 08:11:12 +0900

    :sparkles: Add middlewares to check user's login status

--- prepare/back/routers/user.js
+++ prepare/back/routers/user.js
@@ -3,9 +3,10 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 
 const { Post, User } = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router();
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if (err) {
       console.error(err);
@@ -43,13 +44,13 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
-router.post('/logout', (req, res, next) => {
+router.post('/logout', isLoggedIn, (req, res, next) => {
   req.logout();
   req.session.destroy();
   res.send('ok');
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', isNotLoggedIn, async (req, res, next) => {
   try {
     console.log('req: ', req.body);
     const exUser = await User.findOne({
