43bfcf846763fe1176e21a00d565a3fc4aa090ec
JimmyJoo <jimmyjoo1029@gmail.com>
2020-11-10 10:28:01 +0900

    :sparkles: Update like and unlike post in server

--- prepare/back/routers/post.js
+++ prepare/back/routers/post.js
@@ -22,8 +22,13 @@ router.post('/', isLoggedIn, async (req, res, next) => {
           include: [{ model: User, attributes: ['id', 'nickname'] }],
         },
         {
-          model: User,
+          model: User, // 게시글 작성자
           attributes: ['id', 'nickname'],
+        },
+        {
+          model: User, // 좋아요 누른사람
+          as: 'Likers',
+          attributes: ['id'],
         },
       ],
     });
@@ -36,7 +41,7 @@ router.post('/', isLoggedIn, async (req, res, next) => {
 
 router.delete('/', (req, res) => {});
 
-router.post(`/:postId/comment`, isLoggedIn, async (req, res) => {
+router.post(`/:postId/comment`, isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findOne({
       where: { id: req.params.postId },
@@ -60,6 +65,36 @@ router.post(`/:postId/comment`, isLoggedIn, async (req, res) => {
     });
 
     res.status(201).send(fullComment);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.patch(`/:postId/like`, isLoggedIn, async (req, res, next) => {
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.postId },
+    });
+    if (!post) {
+      return res.status(403).send('존재하지 않는 게시글입니다.');
+    }
+    await post.addLikers(req.user.id);
+    res.send({ PostId: post.id, UserId: req.user.id });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.delete('/:postId/like', isLoggedIn, async (req, res, next) => {
+  try {
+    const post = await Post.findOne({ where: { id: req.params.postId } });
+    if (!post) {
+      return res.status(403).send('존재하지 않는 게시글입니다.');
+    }
+    await post.removeLikers(req.user.id);
+    res.status(200).send({ PostId: post.id, UserId: req.user.id });
   } catch (error) {
     console.error(error);
     next(error);
--- prepare/back/routers/posts.js
+++ prepare/back/routers/posts.js
@@ -8,10 +8,7 @@ router.get('/', async (req, res, next) => {
   try {
     const posts = await Post.findAll({
       limit: 10,
-      order: [
-        ['createdAt', 'DESC'],
-        [Comment, 'createdAt', 'DESC'],
-      ],
+      order: [['createdAt', 'DESC']],
       include: [
         {
           model: User,
@@ -20,7 +17,21 @@ router.get('/', async (req, res, next) => {
         {
           model: Image,
         },
-        { model: Comment, include: [{ model: User }] },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ['id', 'nickname'],
+              order: [['createdAt', 'DESC']],
+            },
+          ],
+        },
+        {
+          model: User,
+          as: 'Likers',
+          attributes: ['id'],
+        },
       ],
     });
     res.status(200).send(posts);
